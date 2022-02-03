bdfa572a9e7d8c57968958716c561ac2d18c62b3
Rukundo Kevin <kevinrukundo1@gmail.com>
2022-02-03 12:35:04 +0200

    Work on article endpoints

---
+++ server/index.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const articleRoutes = require("./routes/article")
+const loginRouter = require("./routes/login")
+const signupRouter = require("./routes/sign-up")
+
+const port = process.env.NODE_ENV || 5000;
+const config = require("config")
+
+const app = express();
+
+mongoose.connect(config.DBHost, {useNewUrlParser: true});
+
+const  db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+mongoose
+
+//middlewares
+app.use(express.json())
+
+//middlewares for routes
+app.use("/article",articleRoutes)
+app.use("/login", loginRouter)
+app.use("/signup", signupRouter)
+
+
+app.set("port",port)
+app.listen(port, () =>{
+        console.log("This server has  started")
+})
